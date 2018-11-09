56103dd0c4d6da672387d08a5b687a129f872b3b
duncanzt <duncanzt@rose-hulman.edu>
2018-11-08 20:49:04 -0500

    position of arm and claw

--- src/duncanzt.py
+++ src/duncanzt.py
@@ -9,16 +9,16 @@ import time
 robot = rb.Snatch3rRobot()
 
 
-def polygon(n):
-    for k in range(n):
-        robot.DriveSystem.go_straight_inches(6)
-        robot.DriveSystem.turn_degrees((n-2)*(180/n))
-
-
-
-
-
-def arm_and_claw():
-    robot.arm.move_arm_to_position(3)
-
-arm_and_claw()
+# def polygon(n):
+#     for k in range(n):
+#         robot.DriveSystem.go_straight_inches(6)
+#         robot.DriveSystem.turn_degrees((n-2)*(180/n))
+#
+#
+#
+#
+#
+# def arm_and_claw():
+#     robot.arm.move_arm_to_position(3)
+#
+# arm_and_claw()
